eb2366166dda81c26e44f6dc3c6012372396c0e7
Pranav Ghate <pranavghate94@gmail.com>
2017-10-04 14:49:37 -0700

    Added Migration and Installation script

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,6 +15,12 @@ module.exports = (app) => {
     //     })
     // });
 
+    app.use((req, res, next)=>{
+        res.header("Access-Control-Allow-Origin","*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/',(req, res)=>{
         res.status(200).send({
             message : 'Welcome to CICERO API'
